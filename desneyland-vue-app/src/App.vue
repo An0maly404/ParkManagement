9694ac08487d832ab9a5b5cbce15b7dd8703791e
LorenzoLeMoineau <lorenzo.lavalette@efrei.net>
2024-11-20 11:15:32 +0000

    Vue.js

---
+++ desneyland-vue-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <a id="title" href="App.vue">DESNEYLAND</a>
+  </header>
+  
+  <div class="header">
+    <nav>
+      <ul>
+        <li><a href="">DESNEYLAND</a></li>
+      </ul>
+    </nav>
+  </div>
+  <p>Here is our web application for our attraction parc management project</p>
+</template>
+
+<script>
+
+</script>
+
+<style>
+header{
+  height: auto;
+  background-color: blue;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
