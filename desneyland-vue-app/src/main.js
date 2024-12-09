e52a9939f0c8e226c490d9adc89549d419565dcc
An0 <ainine.nassim@gmail.com>
2024-12-09 15:55:09 +0000

    Scroll to the top when changing pages !
    Shouldn't have taken as much as it did...

--- desneyland-vue-app/src/main.js
+++ desneyland-vue-app/src/main.js
@@ -1,35 +1,42 @@
-import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-import App from './App.vue'
-import AboutPage from './pages/AboutPage.vue'
-import AttractionsList from './pages/AttractionsList.vue'
-import HotelsList from './pages/HotelsList.vue'
-import LoginPage from './pages/LoginPage.vue'
-import RestaurantsList from './pages/RestaurantsList.vue'
-import TicketInfos from './pages/TicketInfos.vue'
-import HomePage from './pages/HomePage.vue'
-import TicketsOwned from './pages/TicketsOwned.vue'
-import BuyTickets from './pages/BuyTickets.vue'
+import App from './App.vue';
+import AboutPage from './pages/AboutPage.vue';
+import AttractionsList from './pages/AttractionsList.vue';
+import HotelsList from './pages/HotelsList.vue';
+import LoginPage from './pages/LoginPage.vue';
+import RestaurantsList from './pages/RestaurantsList.vue';
+import TicketInfos from './pages/TicketInfos.vue';
+import HomePage from './pages/HomePage.vue';
+import TicketsOwned from './pages/TicketsOwned.vue';
+import BuyTickets from './pages/BuyTickets.vue';
 
-
+// Define the router with routes and scrollBehavior
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: HomePage},
+        { path: '/', component: HomePage },
         { path: '/aboutpage', component: AboutPage },
         { path: '/attractionslist', component: AttractionsList },
         { path: '/hotelslist', component: HotelsList },
         { path: '/loginpage', component: LoginPage },
-        { path: '/restaurantsList', component: RestaurantsList },
+        { path: '/restaurantslist', component: RestaurantsList },
         { path: '/ticketinfos', component: TicketInfos },
-        { path: '/ticketsOwned', component: TicketsOwned},
-        { path: '/buytickets', component: BuyTickets}
-    ]
+        { path: '/ticketsowned', component: TicketsOwned },
+        { path: '/buytickets', component: BuyTickets },
+    ],
+    // Disable ESLint "no-unused-vars" for this function
+    // eslint-disable-next-line no-unused-vars
+    scrollBehavior(to, from, savedPosition) {
+        // Always scroll to the top of the page
+        return { top: 0, behavior: 'smooth' };
+    },
 });
 
-const app = createApp(App)
+// Create and mount the Vue app
+const app = createApp(App);
 
 app.use(router);
 
-app.mount('#app')
+app.mount('#app');
